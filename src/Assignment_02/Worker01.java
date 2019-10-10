ae50d37a7176563b1b3da161a62ea4c7f24557bc
EveaZerin <56233155+EveaZerin@users.noreply.github.com>
2019-10-11 03:02:08 +0600

    Update Worker01.java

--- src/Assignment_02/Worker01.java
+++ src/Assignment_02/Worker01.java
@@ -5,6 +5,6 @@ public class Worker01 {
         return x*2;
     }
     public static int calculate(int x,int y,int z) {
-        return x;
+        return x;              //return "Result is " + (x+y-z);
     }
 }
