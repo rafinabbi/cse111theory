b89e0211ed56af382e2b71bbc5b50af54bc2e899
unknown <Rawhatur>
2019-11-29 22:53:50 +0600

    comment

--- src/Assignment_05/MyString.java
+++ src/Assignment_05/MyString.java
@@ -1,83 +1,266 @@
 package Assignment_05;
 
 public class MyString {
-    char[] c;
 
-    public MyString(String s) {
-        c = s.toCharArray();
+    public String s;
+    char c[];
+
+    public MyString(){
+
     }
 
-    public int length() {
+    public MyString(String s){
+        this.s = s;
+        c=s.toCharArray();
+    }
+
+    public MyString(char []c){
+        this.c= c;
+    }
+
+    public int length(){
         return c.length;
     }
 
-    public Character charAt(int a) {
-        return c[a];
+    public char charAt(int index){
+        return c[index];
     }
 
-    public boolean startsWith(MyString a) {
-        for (int i = 0; i < a.c.length; i++) {
-            if (c[i] == a.c[i]) {
-                continue;
-            }
-            else{
+    public boolean startsWith(String prefix){
+        for (int i = 0; i <prefix.length() ; i++) {
+            if(s.charAt(i)!=prefix.charAt(i))
                 return false;
-            }
         }
         return true;
     }
-    public boolean endsWith(MyString a) {
-        int x=this.c.length-1;
-        for (int i = a.c.length-1; i >0; i--) {
-            if (a.c[i] == this.c[x--]) {
-                continue;
-            }
-            else{
+
+    public boolean startsWith(MyString prefix){
+        for (int i = 0; i <prefix.length() ; i++) {
+            if(s.charAt(i)!=prefix.charAt(i))
                 return false;
-            }
         }
         return true;
     }
-    public String replaceFirst(char a,char b){
+
+    public boolean endsWith(MyString suffix){
+        for (int i = suffix.length()-1; i >-1 ; i--) {
+            if(s.charAt(i)!=suffix.charAt(i))
+                return false;
+        }
+        return true;
+    }
+
+    public String replaceFirst(char old, char n){
         for (int i = 0; i <c.length ; i++) {
-            if(c[i]==a){
-                c[i]=b;
+            if(c[i]==old){
+                c[i]=n;
                 break;
             }
         }
-        return new String(c);
+        String s="";
+        for (int i = 0; i <c.length ; i++) {
+            s+=c[i];
+        }
+        return s;
     }
 
-    public char replaceAll(char a,char b){
+    public String replaceAll(char old, char n){
         for (int i = 0; i <c.length ; i++) {
-            if(c[i]==a){
-                c[i]=b;
+            if(c[i]==old){
+                c[i]=n;
             }
         }
-        return b;
-    }
-    public char[] toLowerCase() {
-        for (int i = 0; i < c.length; i++) {
-            if(c[i]>=65&&c[i]<=90){
-                c[i]=(char)(c[i]+32);
-            }
-        }
-        return c;
-    }
-    public char[] toUpperCase(){
-        for (int i = 0; i < c.length; i++) {
-            if((int)c[i]>=97&&(int)c[i]<=122){
-                c[i]=(char)(c[i]-32);
-            }
-        }
-        return c;
-    }
-    public boolean equals(MyString s){
+        String s="";
         for (int i = 0; i <c.length ; i++) {
-            if(c[i]!=s.charAt(i)) {
-                return false;
+            s+=c[i];
+        }
+        return s;
+    }
+
+    public String replaceLast(char old, char n){
+        for (int i = c.length-1; i >-1 ; i--) {
+            if(c[i]==old){
+                c[i]=n;
+                break;
+            }
+        }
+        String s="";
+        for (int i = 0; i <c.length ; i++) {
+            s+=c[i];
+        }
+        return s;
+    }
+
+    public boolean equalsIgnoreCase(MyString s){
+        for (int i = 0; i <c.length ; i++) {
+            if ((int) c[i] > 64 && (int) c[i] < 91){
+                if (c[i] != s.charAt(i) || (int)c[i] != ((int)s.charAt(i)+32))
+                    return false;
+            }
+            else if ((int)c[i]>96&&(int)c[i]<123){
+                if (c[i] != s.charAt(i )|| (int)c[i] != ((int)s.charAt(i)-32))
+                    return false;
             }
         }
         return true;
+    }
+
+    public boolean equals(MyString s){
+        for (int i = 0; i <c.length ; i++) {
+            if(c[i]!=s.charAt(i))
+                return false;
+        }
+        return true;
+    }
+
+    public String toUpperCase(){
+        char c[]=this.c;
+        for (int i = 0; i <c.length ; i++) {
+            if ((int)c[i]>96&&(int)c[i]<123)
+                c[i]=(char)((int)c[i]-32);
+        }
+        return toString(c);
+    }
+
+    public String toLowerCase(){
+        char c[]=this.c;
+        for (int i = 0; i <c.length ; i++) {
+            if ((int)c[i]>64&&(int)c[i]<91)
+                c[i]=(char)((int)c[i]+32);
+        }
+        return toString(c);
+    }
+
+    public String toString(){
+        return this.s;
+    }
+
+    public String toString(char c[]){
+        String s="";
+        for (int i = 0; i <c.length ; i++) {
+            s+=c[i];
+        }
+        return s;
+    }
+
+    public String subString(int start, int end){
+        String s="";
+        for (int i = start; i <end ; i++) {
+            s+=c[i];
+        }
+        return s;
+    }
+
+    public String subString(int start){
+        return subString(start,c.length-1);
+    }
+
+    public int indexOf(char c){
+        for (int i = 0; i <this.c.length ; i++) {
+            if(this.c[i]==c)
+                return i;
+        }
+        return -1;
+    }
+
+    public int lastIndexOf(char c){
+        for (int i = this.c.length-1; i >-1 ; i--) {
+            if(this.c[i]==c)
+                return i;
+        }
+        return -1;
+    }
+
+    public boolean contains(char c[]){
+        for (int i = 0; i <this.c.length ; i++) {
+            if(this.c[i]==c[0]){
+                for (int j = 1; j <c.length ; j++) {
+                    if(c[j]!=this.c[i++])
+                        return false;
+                }
+            }
+            if(i==this.c.length-1)
+                return false;
+        }
+        return true;
+    }
+
+    public String trim(){
+        String s="";
+        int n=-1;
+        for (int i = 0; i <c.length ; i++) {
+            if(this.c[i]==' ' && n>-1)
+                continue;
+            else if(c[this.c.length-i-1]==' ')
+                continue;
+            n=i;
+            s+=c[i];
+        }
+        return s;
+    }
+
+    public String concat(MyString s1, MyString s2){
+        return s1.s+s2.s;
+    }
+
+    public boolean isEmpty(){
+        return s.length()==0;
+    }
+
+    public int compareTo(MyString s){
+        if (this.equals(s))
+            return 0;
+        else if (this.charAt(0)>s.charAt(0))
+            return +1;
+        else
+            return -1;
+    }
+    public int compareToIgnoreCase(MyString s){
+        String n1= this.s.toLowerCase();
+        String n2= s.toLowerCase();
+        if (n1.equals(n2))
+            return 0;
+        else if (n1.charAt(0)>n2.charAt(0))
+            return +1;
+        else
+            return -1;
+
+    }
+
+    public int indexOf(char c, int index){
+        for (int i = index; i <this.c.length ; i++) {
+            if(this.c[i]==c)
+                return i;
+        }
+        return -1;
+    }
+
+    public int lastIndexOf(char c, int index){
+        for (int i = this.c.length-1; i >=index ; i--) {
+            if(this.c[i]==c)
+                return i;
+        }
+        return -1;
+    }
+
+    public MyString replace(String o, String n){
+        MyString current = new MyString(o),
+                next = new MyString(n);
+
+        for (int i = 0; i <this.s.length() ; i++) {
+            if (this.s.charAt(i)==current.charAt(0)){
+                MyString temp = new MyString(this.s.substring(i,i+current.length()));
+                if (temp.equals(current)){
+                    MyString part1 = new MyString(this.s.substring(0,i)),
+                            remaining = new MyString(this.s.substring(i+current.length(),this.s.length()));
+
+                    this.s = concat(part1,next);
+                    this.s = concat(new MyString(this.s),remaining);
+                    i+=current.length();
+                }
+            }
+        }
+        this.c=this.s.toCharArray();
+        return new MyString(this.s);
     }
 }
